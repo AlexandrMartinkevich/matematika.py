d657ef72bc2c029467bd6918bd7bb523ae4a7b4c
alexandr <wmsuvorov@gmail.com>
2022-12-13 01:36:05 +0200

    математические преобразования

---
+++ numberr.py
@@ -0,0 +1,30 @@
+
+
+number = 1**100
+print('1**100 =', number)
+
+number = 2 ** 16
+print('2**16 =', number)
+
+number = 2-1000
+print('2-1000 =', number)
+
+number = 2+2*2
+print('2+2*2 =', number)
+
+number = 2+5*14
+print('2+5*14 =', number)
+
+number = 60//3
+print('60//3 =', number)
+
+import math
+print('корень квадратный из 144 =', round(math.sqrt(144)))
+
+print('2+5*14=x', 'x =', number)
+
+r = 66-11
+print('66-x=11', 'x =', r)
+
+c = 'x**2+8=16'
+print(c, 'x = 8')
